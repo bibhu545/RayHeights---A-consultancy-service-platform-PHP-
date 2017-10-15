0022e1cc40296a657c1f910cab65909b2f0bf635
Bibhu Ranjan <bibhuranjan.500@gmail.com>
2017-10-16 01:24:12 +0530

    Add files via upload

---
+++ finance/home.php
@@ -0,0 +1,74 @@
+<?php include('includes/header.php'); ?>
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Jumbotron Header -->
+      <header class="jumbotron my-4 home_head" style="font-family: 'Lobster', cursive;">
+    
+        <center><h1 class="">A Warm Welcome to <span style="color: red">R</span>ay<span style="color: red">H</span>eights</h1>
+          <br>
+          <h3>A complete Solution to all Financial Issues</h3>
+        <p class="lead"></p>
+        <a href="../services.php" class="btn btn-primary btn-lg buttonAnm">Our All Services</a></center>
+      
+      </header>
+
+      <!-- Page Features -->
+      <div class="row text-center">
+
+        <div class="col-lg-3 col-md-6 mb-4" style="height: 450px">
+          <div class="card">
+            <img class="card-img-top" src="../images/car_loan.png" alt="car loan">
+            <div class="card-body">
+              <h4 class="card-title">Car Loan</h4>
+              <p class="card-text">Own a vehicle? Expand your level of comfortify as we provide best vehicle loan consultations.</p>
+            </div>
+            <div class="card-footer">
+              <a href="car_loan.php" class="btn btn-primary">Apply Now!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4" style="height: 450px">
+          <div class="card">
+            <img class="card-img-top" src="../images/HomeLoan2.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Home Loan</h4>
+              <p class="card-text">Buying a home is one of the biggest financial investment you may make in your lifetime.We provide secured housing finance at affordable cost.</p>
+            </div>
+            <div class="card-footer">
+              <a href="home_loan.php" class="btn btn-primary">Apply Now!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4" style="height: 450px">
+          <div class="card">
+            <img class="card-img-top" src="../images/personal_loan.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Personal Loan</h4>
+              <p class="card-text">Personal loans act as the best-suited option to smoothen up cash flow at times of stringency.</p>
+            </div>
+            <div class="card-footer">
+              <a href="personal_loan.php" class="btn btn-primary">Apply Now!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4" style="height: 450px">
+          <div class="card">
+            <img class="card-img-top" src="../images/business_loan.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Business Loan</h4>
+              <p class="card-text">Individuals who wish to start their own businesses but do not have the required funds to commence operations.Here we provide best solutions to your need.</p>
+            </div>
+            <div class="card-footer">
+              <a href="business_loan.php" class="btn btn-primary">Apply Now!</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+<?php include('includes/footer.php'); ?>
---
+++ finance/home_loan.php
@@ -0,0 +1,88 @@
+<?php include('includes/header.php'); ?>
+<?php include('../includes/Database.php'); ?>
+<?php
+
+  
+  if(isset($_REQUEST["submit"]))
+  {
+      $firstname = mysqli_real_escape_string($db,$_REQUEST["firstname"]);
+      $phone = mysqli_real_escape_string($db,$_REQUEST["phone"]);
+      $email = mysqli_real_escape_string($db,$_REQUEST["email"]);
+      $price = mysqli_real_escape_string($db,$_REQUEST["price"]);
+      $addr = mysqli_real_escape_string($db,$_REQUEST["addr"]);
+      
+      $date = date("Y-m-d h:i:sa");
+      $emailid='users@rayheights.com';
+      //$to="bibhuranjan.500@gmail.com";
+      $to="office@rayheights.com";
+      $subject="Finance :: Home Loan on date : ".$date;
+      $msg="<html><body>Sender : ".$email."<br> Name : ".$firstname."<br>Phone : $phone<br>Address : $addr<br><br>Price Range: ".$price."<br>Description : $desc<br></body></html>";
+      //echo $emailid;
+      //echo $msg;
+       $headers  =  "From:".$emailid."\r\n".'MIME-Version: 1.0' . "\r\n";
+       $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+       
+       $result= mail($to, $subject, $msg, $headers);
+       if($result)
+       {
+        header('location:home.php?success=Message Sent');
+       }
+  }
+
+?>
+
+    <!-- Page Content -->
+    <div class="container" style="font-size: 18px;">
+
+      <div class="row">
+        <div class="col-xs-0 col-sm-1"></div>
+        <div class="col-xs-12 col-sm-10">
+          <br>
+          <center><h3>We care for your Requirement</h3></center>
+          <form method="post">
+          <div class="form-group">
+            <label>Name : </label>
+            <input type="text" name="firstname" placeholder="Enter Full Name" class="form-control contact" required="required">
+          </div>
+          <div class="form-group">
+            <label>Phone : </label>
+            <input type="text" name="phone" placeholder="Enter Phone Number" class="form-control contact" required="required">
+          </div>
+          <div class="form-group">
+            <label>Email : </label>
+            <input type="email" name="email" placeholder="Enter email here" class="form-control contact" required="required">
+          </div>
+          <div class="form-group">
+            <label>Max Price : </label>
+            <input type="text" name="price" placeholder="Enter a price range or maximum price" class="form-control contact" required="required">
+          </div>
+          <div class="form-group">
+            <label>Address : </label>
+            <textarea name="addr" placeholder="Enter Permanent Address" class="form-control contact" rows="6"></textarea>
+          </div>
+
+
+          <br>
+          <div>
+          <center>
+            <input type="submit" name="submit" value="Submit Requirement" class="btn btn-primary">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="home.php" class="btn btn-danger">Cancel</a>
+          </center>
+          </div>
+        </form>
+          <br>
+        </div>
+        <div class="col-xs-0 col-sm-1"></div>
+      </div>
+      <!-- /.row -->
+
+      <div class="row">
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+<?php include('includes/footer.php'); ?>
